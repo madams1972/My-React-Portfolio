54b1062bf9d94e086f8c0620283ec9b89b8faced
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2023-02-06 22:18:14 -0600

    updated code for gh-pages deployment

--- src/components/data.js
+++ src/components/data.js
@@ -3,38 +3,38 @@ const projectData = [
         id:1,
         projectName:"Horiseon",
         demo:"https://madams1972.github.io/Horiseon-Social-Solution-Services-1/",
-        imgsrc:"/img/horiseon.png"
+        imgsrc:"/horiseon.png"
     },
     {
         id:2,
         projectName:"Password Generator",
         demo:"https://madams1972.github.io/Password-Generator/",
-        imgsrc:"/img/genpass.png"
+        imgsrc:"/genpass.png"
     },
     {
         id:3,
         projectName:"Javascript Quiz",
         demo:"https://madams1972.github.io/javascript-quiz/",
-        imgsrc:"/img/quiz.png"
+        imgsrc:"/quiz.png"
     },
     {
         id:4,
         projectName:"CampAholics",
         demo:"https://madams1972.github.io/javascript-quiz/",
         github:"https://github.com/madams1972/CampAholics/",
-        imgsrc:"/img/camp.png"
+        imgsrc:"/camp.png"
     },
     {
         id:5,
         projectName:"Webdesign Portfolio",
         demo:"https://madams1972.github.io/My-WebDesign-Portfolio/",
-        imgsrc:"/img/portfolio.png"
+        imgsrc:"/portfolio.png"
     },
     {
         id:6,
         projectName:"Nifty Institute",
         demo:"https://niftyinstitute.io/",
-        imgsrc:"/img/nifty.png"
+        imgsrc:"/nifty.png"
     },
 ]
 
